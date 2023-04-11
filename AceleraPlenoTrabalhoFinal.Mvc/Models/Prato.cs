5927c81000887d6bcf8d89556e76b61218c5b3fd
AguiarD <douglasv.aguiar@gmail.com>
2023-04-11 18:50:42 -0300

    Front_Cozinha_MVC_Framework

---
+++ AceleraPlenoTrabalhoFinal.Mvc/Models/Prato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AceleraPlenoTrabalhoFinal.Mvc.Models
+{
+    public class Prato : Entity
+    {
+        public string Descricao { get; set; }
+        public string Foto { get; set; }
+        public decimal Valor { get; set; }
+        public bool Status { get; set; }
+    }
+}
