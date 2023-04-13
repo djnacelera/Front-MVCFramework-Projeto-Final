31202616166cd5ec199a4e99a2b6bf5a65037034
AguiarD <douglasv.aguiar@gmail.com>
2023-04-13 00:30:03 -0300

    Tela Pratos e iniciando o Layout

--- AceleraPlenoTrabalhoFinal.Mvc/Controllers/PratoController.cs
+++ AceleraPlenoTrabalhoFinal.Mvc/Controllers/PratoController.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using AceleraPlenoTrabalhoFinal.Mvc.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +15,65 @@ namespace AceleraPlenoTrabalhoFinal.Mvc.Controllers
 {
     public class PratoController : Controller
     {
-        // GET: Prato
-        public ActionResult Index()
+        private string urlApi = ConfigurationManager.AppSettings["apiPrato"];
+
+        public PratoController()
         {
-            return View();
+        }
+
+        // GET: Prato
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                string token = GetToken();
+
+                List<Prato> listaPrato = new List<Prato>();
+
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    using (var response = httpClient.GetAsync(urlApi + "/Listar").Result)
+                    {
+                        string apiResponse = response.Content.ReadAsStringAsync().Result;
+                        listaPrato = JsonConvert.DeserializeObject<List<Prato>>(apiResponse);
+                    }
+                }
+
+                foreach (var imagem in listaPrato)
+                {
+                    imagem.ConverterBase64ParaJpg();
+                }
+
+                return View(listaPrato);
+
+                /*var pedidos = _iRepositoryPedido.Listar();
+                return View(pedidos);*/
+            }
+            catch (Exception ex)
+            {
+
+                return View(ex.Message);
+            }
+        }
+
+        public string GetToken()
+        {
+            Token tokenModel = new Token();
+            tokenModel.clienteId = ConfigurationManager.AppSettings["clienteId"];
+            tokenModel.clienteSecret = ConfigurationManager.AppSettings["clienteSecret"];
+            string path = ConfigurationManager.AppSettings["apiToken"];
+            string token = "";
+
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(tokenModel), Encoding.UTF8, "application/json");
+                using (var response = httpClient.PostAsync(path, content).Result)
+                {
+                    token = response.Content.ReadAsStringAsync().Result;
+                    return token;//JsonConvert.DeserializeObject<string>(token);
+                }
+            }
         }
     }
 }
--- AceleraPlenoTrabalhoFinal.Mvc/Models/Prato.cs
+++ AceleraPlenoTrabalhoFinal.Mvc/Models/Prato.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -11,5 +13,25 @@ namespace AceleraPlenoTrabalhoFinal.Mvc.Models
         public string Foto { get; set; }
         public decimal Valor { get; set; }
         public bool Status { get; set; }
+        public byte[] Jpg { get; set; }
+
+        public void ConverterBase64ParaJpg()
+        {
+            // converte a string base64 em um array de bytes
+            byte[] imagemBytes = Convert.FromBase64String(Foto);
+
+            // cria um objeto Image a partir do array de bytes
+            using (MemoryStream ms = new MemoryStream(imagemBytes))
+            {
+                Image imagem = Image.FromStream(ms);
+
+                // salva a imagem como arquivo JPEG em um array de bytes
+                using (MemoryStream ms2 = new MemoryStream())
+                {
+                    imagem.Save(ms2, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    Jpg = ms2.ToArray();
+                }
+            }
+        }
     }
 }
